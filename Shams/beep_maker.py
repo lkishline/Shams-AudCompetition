ae27b6d11eed2b3a598889a6f165837d1cd79b80
Lindsey Kishline <lrk4@uw.edu>
2015-04-07 16:41:31 -0700

    Playing with filters and windows now

--- Shams/beep_maker.py
+++ Shams/beep_maker.py
@@ -7,7 +7,7 @@ Created on Thu Apr  2 16:20:52 2015
 
 import numpy as np
 from scipy import io as sio
-from scipy.fftpack import rfft, irfft, fftfreq
+from scipy.fftpack import fft, irfft, fftfreq, fftshift
 import scipy.signal as signal
 import matplotlib.pyplot as plt
 from expyfun import stimuli 
@@ -37,7 +37,7 @@ for x in xrange(1, 5):
     freq = fund*x
     tonecomp = tonecomp + np.sin(freq * 2 * np.pi * np.arange(int(fs * dur)) / float(fs))
     
-plt.plot(tonecomp)
+#plt.plot(tonecomp)
 
 # look in frequency domain ### doesn't work due to fft function ??
 #freqindex = np.arange(int(fs - 1))
@@ -54,7 +54,7 @@ env = env / 2
 #plt.plot(env)
 
 stim = np.multiply(env, tone)
-plt.plot(stim)
+#plt.plot(stim)
 
 
 ### Making white noise ###
@@ -66,7 +66,23 @@ noiseburst = np.random.normal(0, 1.0, int(fs * 1.0))  # 1 secs
 # Filtering with a lowpass cut-off freq of 1500Hz using 100th order Hamming
 #fltnoiseburst = stimuli._filter(noiseburst, )
 
-flteredburst = stimuli.get_env(noiseburst, fs, 100, 1500) # not right obvi
-plt.plot(flteredburst)
+#flteredburst = stimuli.get_env(noiseburst, fs, 100, 1500) # not right obvi
+#plt.plot(flteredburst)
+
+window = signal.hamming(100)
+plt.plot(window)
+plt.title("Hamming window")
+plt.ylabel("Amplitude")
+plt.xlabel("sample")
+
+plt.figure()
+A = fft(window, fs)
+freq = np.linspace(-0.5, 0.5, len(A))
+response = 20 * np.log10(np.abs(fftshift(A / abs(A).max())))
+plt.plot(freq, response)
+plt.axis([-0.5, 0.5, -120, 0])
+plt.title("Frequency response of the Hamming window")
+plt.ylabel("Normalized magnitude [dB]")
+plt.xlabel("Normalized frequency [cycles per sample]")
 
 ### Making two beeps seperated by some amount of time ###

29c5e38492dfde9332a2327be1a593f42572e63e
Lindsey Kishline <lrk4@uw.edu>
2015-04-09 14:28:56 -0700

    getting windows and details right

--- Shams/expyfun_stimgen_play.py
+++ Shams/expyfun_stimgen_play.py
@@ -13,13 +13,16 @@ import matplotlib.pyplot as plt
 fs = 24414.             # Sampling rate - how many samples per second
 dur = 1.0               # duration of stimulus
 
+### Simple tone ###
+
 tone = np.sin(2 * np.pi * 100 * np.arange(int(fs * dur)) / float(fs))
     # a tone at a 100 Hz - just think how often a multiple of 2pi arrives
 tone *= 0.01 * np.sqrt(2)  # Set RMS to 0.01
 
-ramp = 0.006
+toneramp = 0.006
+noiseramp = 0.003
 
-sig = stimuli._stimuli.window_edges(tone, fs, ramp)
+sig = stimuli._stimuli.window_edges(tone, fs, toneramp)
 
 # plt.plot(sig)
 
@@ -31,7 +34,15 @@ for x in xrange(1, 5):
     freq = fund*x
     tonecomp = tonecomp + np.sin(freq * 2 * np.pi * np.arange(int(fs * dur)) / float(fs))
 
-tonecomp_ramped = stimuli._stimuli.window_edges(tonecomp, fs, ramp, -1, 'hamming')
+tonecomp_ramped = stimuli._stimuli.window_edges(tonecomp, fs, toneramp)
 
-plt.plot(tonecomp_ramped)
+# plt.plot(tonecomp_ramped)
 
+
+### noise burst ###
+
+noiseburst = np.random.normal(0, 1.0, int(fs * 1.0))  # 1 secs
+
+nb_ramped = stimuli._stimuli.window_edges(noiseburst, fs, noiseramp, -1, 'hamming')
+
+plt.plot(nb_ramped)

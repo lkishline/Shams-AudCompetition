714a6b337728dd857adce18425c44091d3377e28
Lindsey Kishline <lrk4@uw.edu>
2015-04-14 14:54:31 -0700

    added time index and correct duration

--- Shams/bringing_together.py
+++ Shams/bringing_together.py
@@ -11,21 +11,24 @@ import matplotlib.pyplot as plt
 from expyfun import stimuli
 
 fs = 24414.
-dur = 1.0
+dur = 0.008
+n = int(dur * fs)    # total number of samples
+t = np.linspace(0, dur, n, endpoint=False)    # time index
 
 ### Tone complex ###
-tonecomp = np.zeros(24414, float)
+tonecomp = np.zeros(24414. * dur, float)
 fund = 250.0                                # fundamental frequency
 for x in xrange(1, 5):
     freq = fund*x
     tonecomp = tonecomp + np.sin(freq * 2 * np.pi * np.arange(int(fs * dur)) / float(fs))
 
 ### Making white noise ###
-nb = np.random.normal(0, 1.0, int(fs * 1.0))
+nb = np.random.normal(0, 1.0, int(fs * dur))
 
 ### highpass cut-off freq of 1500Hz using 100th order Hamming ###
 b = sig.firwin(101, 1500. / (fs / 2), pass_zero=False)  # False for highpass
                                                         # nyq_rate = fs / 2
+                                                        # have to add '1' order
 filtered_stim = sig.lfilter(b, 1.0, nb)
 #plt.plot(filtered_stim)
 
@@ -34,9 +37,14 @@ toneramp = 0.006
 noiseramp = 0.003
 
 nb_ramped = stimuli._stimuli.window_edges(nb, fs, noiseramp, -1, 'hamming')
-
 finalstim_nb = np.multiply(nb_ramped, filtered_stim)
-#plt.plot(finalstim_nb)
 
 finalstim_tc = stimuli._stimuli.window_edges(tonecomp, fs, toneramp, -1, 'hamming')
-plt.plot(finalstim_tc)
+
+
+
+### plots to check frequencies and timing/windowing ###
+
+plt.plot(t, finalstim_nb)
+plt.plot(t, finalstim_tc)
+

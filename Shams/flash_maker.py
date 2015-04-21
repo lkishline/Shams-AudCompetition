314c8eab4714a39230608eec3db2cf39c8a1f23f
Lindsey Kishline <lrk4@uw.edu>
2015-04-21 14:17:42 -0700

    Starting a flash maker...fingers crossed

---
+++ Shams/flash_maker.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 14:14:52 2015
+
+@author: lindsey
+"""
+import numpy as np
+import scipy.signal as sig
+import matplotlib.pyplot as plt
+from expyfun import stimuli
+
+### attempts at making flashes ###
+

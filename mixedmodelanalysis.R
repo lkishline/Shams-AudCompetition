7419ead65052dc17b58e2f8852d7475531d7c37e
Lindsey Kishline <l.kishline@gmail.com>
2017-08-08 16:54:05 -0700

    Add files via upload

--- mixedmodelanalysis.R
+++ mixedmodelanalysis.R
@@ -1,4 +1,5 @@
 library(lme4)
+#library(caret)
 setwd("/home/lindsey/Desktop/PythonFiles/Shams/Flashbeeps2.0")
 
 foo <- read.delim("GLMMData_ecc.tsv",
@@ -7,6 +8,28 @@ foo <- read.delim("GLMMData_ecc.tsv",
                               number_pressed="numeric", raw_RT="numeric", control="numeric",
                               V1A1_or_V2A2="integer", V1A2="integer", V2A1="integer", Vcent="integer",
                               Vperiph_Af_center="integer", Vperiph_Af_periph="integer"))
+# variable notation:
+#
+# V: The visual stimuli (visual flash(es))
+# A: An auditory stimulus
+# Ac: The auditory stimulus that is collocated with the visual flash(es)
+# Af: The auditory stimulus that is not collocated (foil) with the visual flash(es)
+# center/cent: This indicates that a stimulus was located in the central position (one of three positions)
+# type: indicates whether an auditory stimulus was 'noise burst' or 'tone complex'
+
+# variable descriptions:
+#
+# number_pressed: Subjects pressed either "1" or "2" to indicate the number of flashes that they saw
+# cue_attn: indicates whether or not a visual cue appeared prior to trial indicating location of flash(es)
+# V1A1_or_V2A2: (collocated controls) the instances wherein the number of visual events, match the number of events in the collocated auditory stream.
+#               For example: V1A1 indicates that there was one visual flash and the collocated auditory stimuli also had one beep
+#                            V2A2 indicates that there were two visual flashes and the collocated auditory stimulus also had two beeps
+# V1A2: (fission) One visual flash occurred while two auditory beeps occurred in the collocated auditory stream.
+# V2A1: (fusion) Two visual flashes occurred while one auditory beep occurred in the collocated auditory stream.
+# Vcent: The trials in which the visual flash(es) occured in the center location (meaning that the auditory stream that was NOT collocated could only be one space away)
+# Vperiph_Af_center: "Near" trials in which the visual flash(es) occured in the peripheral location and that the auditory stream that was NOT collocated was one space away in the center position.
+# Vperiph_Af_center: "Far" trials in which the visual flash(es) occured in the peripheral location and that the auditory stream that was NOT collocated was two spaces away in the other peripheral position.
+# Ac_type: Indicates if the illusion inducing auditory stream was either a tone or a noise burst
 
 # remove unused columns
 
